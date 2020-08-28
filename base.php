1f366c9a329ce5001bf35ba3e53c1219ceb16a16
Carlos Duque <ang_carlos@hotmail.com>
2020-08-28 12:04:20 -0500

    cambios a una sola pagina

--- base.php
+++ base.php
@@ -29,7 +29,7 @@
         </div>
         <!--         hola -->
         <p>Promotor:</p>
-        <p class="name"> :p </p>
+        <p class="name"> </p>
         <p>
           796 Silver Harbour,<br>
           TX 79273, US
@@ -39,16 +39,17 @@
       </div>
 
       <div class="data right">
-        <div class="title">Cliente</div>
+        <br>
+        <div class="title">Nomre cliente</div>
         <div class="date">
-          Fecha contrato: 01/06/2020<br>
-          limite?: 30/06/2020
+          <p>Fecha contrato: 01/06/2020</p>
+          <p>Vigencia de contrato</p>
         </div>
         <div class="company-info">
           <!-- texto -->
          
           <span class="line"></span>
-          <a class="phone" href="tel:602-519-0450">(602) 519-0450</a>
+     
           <span class="line"></span>
           <a class="email" href="mailto:company@example.com">company@example.com</a>
           <br>
@@ -72,7 +73,7 @@
                 <tr>
                   <th class="no"></th>
                   <th class="desc">
-                    <div>Pago Inicial</div>
+                    <div>Valor equipos y conceptos financieros (sin IVA)</div>
                   </th>
                   <th class="total">
                     <div>Total</div>
@@ -84,7 +85,7 @@
                   <td class="no">-</td>
                   <td class="desc">
                     <div>
-                      <p>Deposito y Garantia</p>
+                      <p>Valor de la unidad</p>
                     </div>
                   </td>
 
@@ -92,34 +93,24 @@
                 </tr>
                 <tr>
                   <td class="no">-</td>
-                  <td class="desc">Por nombrar</td>
+                  <td class="desc">Equipo aliado</td>
 
                   <td class="total">$3,200.00</td>
                 </tr>
                 <tr>
                   <td class="no">-</td>
-                  <td class="desc">Seguro Primer año</td>
+                  <td class="desc">Seguro financiero</td>
                   <td class="total">$800.00</td>
                 </tr>
                 <tr>
                   <td class="no">-</td>
-                  <td class="desc">Gastos de investigacion</td>
-                  <td class="total">$800.00</td>
+                  <td class="desc">(-)Renta extraordinaria</td>
+                  <td class="total">$1,800.00</td>
                 </tr>
                 <tr>
                   <td class="no">-</td>
-                  <td class="desc">Accesorios y otros gastos</td>
-                  <td class="total">$800.00</td>
-                </tr>
-                <tr>
-                  <td class="no">-</td>
-                  <td class="desc">Validacion de cuenta</td>
-                  <td class="total">$800.00</td>
-                </tr>
-                <tr>
-                  <td class="no">-</td>
-                  <td class="desc">Impuesto Valor agregado</td>
-                  <td class="total">$800.00</td>
+                  <td class="desc">Monto Total Financiero</td>
+                  <td class="total">$1,800.00</td>
                 </tr>
               </tbody>
             </table>
@@ -154,33 +145,32 @@
               </tr>
               <tr>
                 <td class="no">-</td>
-                <td class="desc">Por nombrar</td>
-
-                <td class="total">$3,200.00</td>
-              </tr>
-              <tr>
-                <td class="no">-</td>
                 <td class="desc">Seguro Primer año</td>
                 <td class="total">$800.00</td>
               </tr>
               <tr>
                 <td class="no">-</td>
-                <td class="desc">Gastos de investigacion</td>
+                <td class="desc">Renta Extraordinaria</td>
                 <td class="total">$800.00</td>
               </tr>
               <tr>
                 <td class="no">-</td>
-                <td class="desc">Accesorios y otros gastos</td>
+                <td class="desc">Gastos de investigación</td>
                 <td class="total">$800.00</td>
               </tr>
               <tr>
                 <td class="no">-</td>
-                <td class="desc">Validacion de cuenta</td>
+                <td class="desc">Sub total pago inicial</td>
                 <td class="total">$800.00</td>
               </tr>
               <tr>
                 <td class="no">-</td>
                 <td class="desc">Impuesto Valor agregado</td>
+                <td class="total">$800.00</td>
+              </tr>
+              <tr>
+                <td class="no">-</td>
+                <td class="desc">Total Pago Inicial</td>
                 <td class="total">$800.00</td>
               </tr>
             </tbody>
@@ -197,9 +187,8 @@
                 <tr>
                   <th class="no"></th>
                   <th class="desc">
-                    <div> Datos generales</div>
+                    <div>Pagos mensuales</div>
                   </th>
-
                   <th class="total">
                     <div>Total</div>
                   </th>
@@ -210,28 +199,19 @@
                   <td class="no">-</td>
                   <td class="desc">
                     <div>
-                      <p>Deposito y Garantia</p>
-                    </div>
-                    <br>
-                    <div>
-                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                      <p>Renta básica</p>
                     </div>
                   </td>
-
-
                   <td class="total">$1,200.00</td>
                 </tr>
                 <tr>
                   <td class="no">-</td>
-                  <td class="desc">Developing a Content Management System-based Website</td>
-
-
+                  <td class="desc">Pago por seguro</td>
                   <td class="total">$3,200.00</td>
                 </tr>
                 <tr>
                   <td class="no">-</td>
-                  <td class="desc">Optimize the site for search engines (SEO)</td>
-
+                  <td class="desc">Total pago Mensual</td>
                   <td class="total">$800.00</td>
                 </tr>
               </tbody>
@@ -259,25 +239,11 @@
                 <td class="no">-</td>
                 <td class="desc">
                   <div>
-                    <p>Deposito y Garantia</p>
-                  </div>
-                  <br>
-                  <div>
-                    <p>Datos del usuario</p>
+                    <p>Valor residual</p>
                   </div>
                 </td>
-
-
                 <td class="total">$1,200.00</td>
               </tr>
-              <tr>
-                <td class="no">-</td>
-                <td class="desc">Developing a Content Management System-based Website</td>
-
-
-                <td class="total">$3,200.00</td>
-              </tr>
-
             </tbody>
           </table>
         </div>
@@ -331,6 +297,72 @@
       </div>
     </div>
 </section>
+      <section>
+      <div class="details clearfix" style="background-color: transparent;">
+  
+          <div class="table-wrapper">
+            <table id="meses">
+              <tbody class="head">
+                <tr>
+                 
+                  <th class="desc">
+                    <div> Cordialmente </div>
+                  </th>
+
+                     <th class="desc">
+                      <div>Propuesta aceptada por:</div>
+                    </th>
+
+                </tr>
+                
+              </tbody>
+              <tbody class="body">
+                <tr>
+                
+                  <td class="desc">
+                    <div>
+                      <br>
+                    </div>
+                    <br>
+                    <div> 
+                      
+                    </div>
+                  </td>
+
+                   <td class="desc">
+                    <div>
+                      <p><br></p>
+                    </div>
+                    <br>
+                    <div> 
+                      
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  
+                  <td class="desc"><br></td>
+                   <td class="desc"><br></td>
+                </tr>
+                <tr>
+                
+                  <td class="desc">
+                    <hr>
+                    <p>#N/A</p>
+                    AB&C Leasing de Mexico, S.A.P.I de CV</td>
+
+                    <td class="desc">
+                    <hr>
+                    <p>AB&C Leasing de Mexico, S.A.P.I de CV</p>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+        </div>
+     </section>
+
 </body>
 <!-- <pagebreak> -->
 
